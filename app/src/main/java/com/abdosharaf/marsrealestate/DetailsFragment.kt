81ebd1a0f06309b9229b5066df9ebed6ef54b7a0
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-04-05 09:38:17 +0200

    Initialize the app theme & icon, main screens, and navigation

---
+++ app/src/main/java/com/abdosharaf/marsrealestate/DetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.abdosharaf.marsrealestate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abdosharaf.marsrealestate.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
